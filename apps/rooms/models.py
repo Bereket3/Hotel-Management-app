bd4b2c311e05d5a7fe1e3e269bed7e47f252b6b6
Trash Pitch <workumyfather@gmail.com>
2024-09-09 16:26:08 +0300

    feat: hotel and rooms regestration models

--- apps/rooms/models.py
+++ apps/rooms/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Images(models.Model):
+    image = models.ImageField(upload_to="pictures")
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.image.__str__()
+
+
+class RoomsObject(models.Model):
+    room_id = models.CharField(max_length=400)
+    number_of_people = models.IntegerField()
+    images = models.ManyToManyField(Images)
+
+    def __str__(self):
+        return self.room_id.__str__()
